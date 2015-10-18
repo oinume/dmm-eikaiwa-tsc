d2a35a6eed2016bd55b5f1feff872bf0a4699373
Kazuhiro Oinuma <oinume@gmail.com>
2015-10-19 00:56:59 +0900

    Connect to ClearDB MySQL

---
+++ tsc/db.py
@@ -0,0 +1,22 @@
+import os
+import urllib.parse
+import pymysql
+import pymysql.cursors
+
+# Register database schemes in URLs.
+urllib.parse.uses_netloc.append("mysql")
+
+
+def connect() -> pymysql.connections.Connection:
+    url_env = os.environ.get("CLEARDB_DATABASE_URL")
+    if not url_env:
+        raise Exception("Environment 'CLEARDB_DATABASE_URL' is not defined.")
+    url = urllib.parse.urlparse(url_env)
+    return pymysql.connect(
+        host=url.hostname,
+        user=url.username,
+        password=url.password,
+        database=url.path[1:],
+        charset="utf8mb4",
+        autocommit=True,
+        cursorclass=pymysql.cursors.DictCursor)
