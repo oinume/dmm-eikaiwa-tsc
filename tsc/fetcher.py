98f8af6a6d93d2f63114fd44e01d3bdd840f8d3a
Kazuhiro Oinuma <oinume@gmail.com>
2015-10-25 22:00:53 +0900

    Add close() method

--- tsc/fetcher.py
+++ tsc/fetcher.py
@@ -1,10 +1,24 @@
 # coding: utf-8
+from selenium import webdriver
+from pyquery import PyQuery as pq
 
 
 class TeacherScheduleFetcher:
 
     def __init__(self):
-        pass
+        self.browser = webdriver.PhantomJS()
+        #self.browser = webdriver.Firefox()
 
     def fetch(self, teacher_id):
-        pass
+        url_base = "http://eikaiwa.dmm.com/teacher/index/{0}/"
+        b = self.browser
+        b.get(url_base.format(teacher_id))
+        d = pq(b.page_source)
+        title = d("title").text()
+        print(title)
+        # TODO: insert to teacher table
+
+
+    def close(self):
+        self.browser.quit()
+
