e1f5981d34d9bb1657567bee11b83f36bd1afc8a
oinume <oinume@gmail.com>
2015-10-30 00:33:39 +0900

    fetcher doesnt access to database

--- tsc/fetcher.py
+++ tsc/fetcher.py
@@ -5,16 +5,16 @@ import re
 from selenium import webdriver
 import lxml.html
 import tsc.db
+from typing import Any, List, Tuple
 
 
 class TeacherScheduleFetcher:
-
     def __init__(self):
         self.browser = webdriver.PhantomJS()
         #self.browser = webdriver.Firefox()
         self.conn = tsc.db.connect()
 
-    def fetch(self, teacher_id):
+    def fetch(self, teacher_id: int) -> Tuple[str, List[Any]]:
         url_base = "http://eikaiwa.dmm.com/teacher/index/{0}/"
         b = self.browser
         b.get(url_base.format(teacher_id))
@@ -22,34 +22,44 @@ class TeacherScheduleFetcher:
         root = lxml.html.fromstring(b.page_source)
         title = root.xpath("//title")[0].text
         name = title.split("-")[0].strip()
-        with self.conn.cursor() as cursor:
-            cursor.execute(
-                "INSERT INTO teacher VALUES (%s, %s) ON DUPLICATE KEY UPDATE name=%s",
-                (teacher_id, name, name,)
-            )
+        # with self.conn.cursor() as cursor:
+        #     cursor.execute(
+        #         "INSERT INTO teacher VALUES (%s, %s) ON DUPLICATE KEY UPDATE name=%s",
+        #         (teacher_id, name, name,)
+        #     )
 
+        # schedule, reservation
         date = datetime.date.today()
         time_items = root.xpath("//ul[@class='oneday']//li")
+        schedules = []
         for time_item in time_items:
             time_class = time_item.attrib["class"]
             text = time_item.text_content().strip()
             #print("{time_class}:{text}".format(**locals()))
             # blank, reservable, reserved
-            #date_str = ""
             if time_class == "date":
                 match = re.match(r"([\d]+)月([\d]+)日(.+)", text)
                 if match:
                     date = date.replace(date.year, int(match.group(1)), int(match.group(2)))
-                    #print(date)
             elif time_class.startswith("t-") and text != "":
                 tmp = time_class.split("-")
                 dt = datetime.datetime(date.year, date.month, date.day, int(tmp[1]), int(tmp[2]), 0, 0)
-                status = "reservable" if text == "予約済" else "reserved"
+                if text == "終了":
+                    status = "finished"
+                elif text == "予約済":
+                    status = "reserved"
+                elif text == "予約可":
+                    status = "reservable"
+                else:
+                    raise(Exception("Unknown schedule text:{0}".format(text)))
                 print("{dt}:{status}".format(**locals()))
-                # TODO: INSERT TO DB or append list
+                schedules.append({
+                    "datetime": dt,
+                    "status": status,
+                })
             else:
-                #print("else")
                 pass
+        return name, schedules
 
     def close(self):
         self.conn.close()
