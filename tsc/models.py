8a960287ddb87431e513751b53a65e84fba8e729
Kazuhiro Oinuma <oinume@gmail.com>
2015-11-04 00:47:55 +0900

    refactor

--- tsc/models.py
+++ tsc/models.py
@@ -8,21 +8,13 @@ class Teacher:
         self.id = id
         self.name = name
 
-    # def get_name(self):
-    #     return self._name
-    #
-    # def set_name(self, value):
-    #     self._name = value
-    #
-    # name = property(get_name, set_name, None, "'name' property")
-
 
 ScheduleStatus = enum.Enum("ScheduleStatus", "reservable reserved finished")
 
 
 class Schedule:
 
-    def __init__(self, teacher_id: int, datetime: datetime.datetime, status: ScheduleStatus):
+    def __init__(self, teacher_id: int, dt: datetime.datetime, status: ScheduleStatus):
         self.teacher_id = teacher_id
-        self.datetime = datetime
+        self.datetime = dt
         self.status = status
