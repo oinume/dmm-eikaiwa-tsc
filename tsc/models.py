77115a8a85cd4be9d385a974ce7a37ded121d540
Kazuhiro Oinuma <oinume@gmail.com>
2015-11-06 02:33:03 +0900

    First implementation

--- tsc/models.py
+++ tsc/models.py
@@ -8,6 +8,9 @@ class Teacher:
         self.id = id
         self.name = name
 
+    def __repr__(self) -> str:
+        return "<Teacher({0}, {1})>".format(self.id, self.name)
+
 
 ScheduleStatus = enum.Enum("ScheduleStatus", "reservable reserved finished")
 
@@ -18,6 +21,9 @@ class Schedule:
         self.teacher_id = teacher_id
         self.datetime = dt
         self.status = status
+
+    def __repr__(self) -> str:
+        return "<Schedule({0}, {1}, {2})>".format(self.teacher_id, self.datetime, self.status.name)
 
 
 class DBMapper:
@@ -44,3 +50,27 @@ class DBMapper:
             sql = sql[:-1]
             sql += " ON DUPLICATE KEY UPDATE status=VALUES(status)"
             cursor.execute(sql, values)
+
+    def find_schedules(
+        self,
+        teacher_id: int,
+        from_date: datetime.date,
+        to_date:datetime.date
+    ) -> List[Schedule]:
+        with self._conn.cursor() as cursor:
+            sql = """
+SELECT * FROM schedule
+WHERE
+  teacher_id = %s
+  AND DATE(datetime) BETWEEN %s AND %s
+""".strip()
+            cursor.execute(
+                sql,
+                (teacher_id, from_date.strftime("%Y-%m-%d"), to_date.strftime("%Y-%m-%d"),)
+            )
+            print(cursor._last_executed)
+
+            schedules = []
+            for row in cursor.fetchall():
+                schedules.append(Schedule(row["teacher_id"], row["datetime"], ScheduleStatus(row["status"])))
+            return schedules
